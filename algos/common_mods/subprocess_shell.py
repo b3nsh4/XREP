199ff3059e2e80182152be138fafc60584359042
ben shaji <benshaji888@gmail.com>
2022-04-24 10:08:08 +0530

    fix: Subprocess output includes args

---
+++ algos/common_mods/subprocess_shell.py
@@ -0,0 +1,30 @@
+from echo_escape import echo_escaper
+import subprocess
+def run_shell(req):
+   patt1 = req['patt1']
+   patt2 = req['patt2']
+   patt3 = req['patt3']
+   patt4 = req['patt4']
+   patt5 = req['patt5']
+   patt6 = req['patt6']
+   full_line = req['full_line'][0]
+   fresh_echo=echo_escaper(full_line)
+   print("->",fresh_echo)
+   patt1_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt1,shell=True).decode()
+   patt2_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt2,shell=True).decode()
+   patt3_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt3,shell=True).decode()
+   patt4_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt4,shell=True).decode()
+   if patt5!="Works_better_with_complex":
+      patt5_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt5,shell=True).decode()
+   else:
+      patt5_op=" "
+   patt6_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt6,shell=True).decode()
+   final_res = {
+      "patt1_shell_op":patt1_op,
+      "patt2_shell_op":patt2_op,
+      "patt3_shell_op":patt3_op,
+      "patt4_shell_op":patt4_op,
+      "patt5_shell_op":patt5_op,
+      "patt6_shell_op":patt6_op,
+   }
+   return final_res
--- xrep.py
+++ xrep.py
@@ -537,39 +537,12 @@ def bug_report():
    report_status=init_report(collect_report_vars)
    return report_status
 
-from echo_escape import echo_escaper
-import subprocess
+
+from subprocess_shell import run_shell
 @app.route('/run_test',methods=["POST","GET"])
 def run_with_sed():
    req = request.get_json()
-   patt1 = req['patt1']
-   patt2 = req['patt2']
-   patt3 = req['patt3']
-   patt4 = req['patt4']
-   patt5 = req['patt5']
-   patt6 = req['patt6']
-   full_line = req['full_line'][0]
-   fresh_echo=echo_escaper(full_line)
-   print("->",fresh_echo)
-   patt1_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt1,shell=True).decode()
-   patt2_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt2,shell=True).decode()
-   patt3_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt3,shell=True).decode()
-   patt4_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt4,shell=True).decode()
-   if patt5!="Works_better_with_complex":
-      patt5_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt5,shell=True).decode()
-   else:
-      patt5_op=" "
-   patt6_op = subprocess.check_output('echo -e "'+fresh_echo+'" |'+patt6,shell=True).decode()
-   final_res = {
-      "patt1_shell_op":patt1_op,
-      "patt2_shell_op":patt2_op,
-      "patt3_shell_op":patt3_op,
-      "patt4_shell_op":patt4_op,
-      "patt5_shell_op":patt5_op,
-      "patt6_shell_op":patt6_op,
-   }
-   return final_res
-
+   return run_shell(req)
 
 @app.route('/donate')
 def donate_xrep():
