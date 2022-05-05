cc39695b29d10d1655d5e14ab1771e97f4cadc92
ben shaji <benshaji888@gmail.com>
2022-05-05 17:41:15 +0530

    Improvements to Patt1_static_brd on wildcard usages

--- algos/special_patterns/patt1_static_brd.py
+++ algos/special_patterns/patt1_static_brd.py
@@ -32,15 +32,64 @@ def patt1_static_str(lhs,rhs,cooked_string,LINE_NUM,pre_has_space):
 		pre_res = lhs_static_str(lhs)
 		post_res = rhs_static_str(rhs)
 		pre1,pre2 = pre_res[0],pre_res[1]
+		
+		if pre1=="":
+			pre1=""
+		else:
+			pre1=pre1+".*"
+		
+		if pre_has_space==True:
+			pre2=pre2+".*"
+		else:
+			pre2=pre2
+
 		post1,post2 = post_res[0],post_res[1]
-		return f"sed -E -n '{LINE_NUM}s/.*"+pre1+".*"+pre2+".*"+"("+p_space+(cooked_string)+").*"+post1+".*"+post2+"/\\1/p'"
+		return f"sed -E -n '{LINE_NUM}s/.*"+pre1+pre2+"("+p_space+(cooked_string)+").*"+post1+".*"+post2+"/\\1/p'"
 		
 	elif len(lhs)==0 and len(rhs)!=0:
 		post_res = rhs_static_str(rhs)
 		post1,post2 = post_res[0],post_res[1]
-		return f"sed -E -n '{LINE_NUM}s/.*("+p_space+(cooked_string)+").*"+post1+".*"+post2+".*/\\1/p'"
+		
+		if pre_has_space==True:
+			init_with = ".*"
+		else:
+			init_with = "^"
+		
+		if post1!="":
+			post1=post1+".*"
+		if post2!="":
+			post2=post2+".*"
+		
+		return f"sed -E -n '{LINE_NUM}s/{init_with}("+p_space+(cooked_string)+").*"+post1+post2+"/\\1/p'"
 	
 	elif len(lhs)!=0 and len(rhs)==0:
 		pre_res = lhs_static_str(lhs)
 		pre1,pre2 = pre_res[0],pre_res[1]
-		return f"sed -E -n '{LINE_NUM}s/.*"+pre1+".*"+pre2+".*("+p_space+(cooked_string)+").*/\\1/p'"
+		
+		if pre1=="":
+			pre1=""
+		else:
+			pre1=pre1+".*"
+
+		if pre_has_space==True:
+			pre2=pre2+"" #REF-psb-01
+		else:
+			pre2=pre2
+		
+		return f"sed -E -n '{LINE_NUM}s/.*"+pre1+pre2+"("+p_space+(cooked_string)+").*/\\1/p'"
+
+
+"""
+- Adding unnecessary .* causing wrong output
+- we carefully add .* to pre2 only if p_space is True
+- Since we have \\s+ infront of target \\s+(target) , we will get it fine
+- But if i include a wildcard .*\\s*(target), then we will not get expected
+- In those cases we use no wildcards assuming target and custom-brd is a same word
+- example -> process 117 is (dead) now
+- exmaple -> process 186 is (alive) now
+- we can choose ( and ) as custom-brd, it's clear that it's one single word
+- We should NOT use any wildcards here
+- Post can/cannot have wildcards (which may be fine)
+- It was also not good, eventho pre1 doesnot exist, hardcoding .* wildcard was appearing always
+- psb-01: we cannot add a wildcard .* since no Postb exist will not get expected as its goes till end
+"""
