239917d2712530f3cfea6b998760ba41bf91c058
ben shaji <benshaji888@gmail.com>
2022-06-13 10:13:30 +0530

    Fixes in Pyre RunTest, skips errors, shows the rest

--- algos/common_mods/pyre_shell.py
+++ algos/common_mods/pyre_shell.py
@@ -11,15 +11,33 @@ def run_pyre_shell(req):
 	full_line = req['full_line'][0]
 	TXT=echo_escaper(full_line)
 
-	patt1_shell_op=str(eval(patt1).groups()),
-	patt2_shell_op=str(eval(patt2).groups()),
-	patt3_shell_op="___",
+	try:
+		patt1_shell_op=str(eval(patt1).groups()),
+	except AttributeError:
+		patt1_shell_op="Something went wrong, please report this!"
+	
+	try:
+		patt2_shell_op=str(eval(patt2).groups()),
+	except AttributeError:
+		patt2_shell_op = "Something went wrong, please report this!"
+	
 	if patt4!="long_result_ignored":
-		patt4_shell_op=str(eval(patt4).groups()),
+		try:
+			patt4_shell_op=str(eval(patt4).groups()),
+		except AttributeError:
+			patt4_shell_op = "Something went wrong, please report this!"
 	else:
 		patt4_shell_op="long_result_ignored"
-	patt5_shell_op=str(eval(patt5).groups()),
-	patt6_shell_op=str(eval(patt6).groups())
+	
+	try:
+		patt5_shell_op=str(eval(patt5).groups()),
+	except AttributeError:
+		patt5_shell_op = "Something went wrong, please report this!"
+	
+	try:
+		patt6_shell_op=str(eval(patt6).groups())
+	except AttributeError:
+		patt6_shell_op = "Something went wrong, please report this!"
 	
 	final_res = {
 	"patt1_shell_op":patt1_shell_op,
