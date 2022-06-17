3a4d98f25727aeb6f6b744f3b7b9dfb74b61e4aa
ben shaji <benshaji888@gmail.com>
2022-06-17 14:27:18 +0530

    Minor fixes

--- algos/pattern_6_beta.py
+++ algos/pattern_6_beta.py
@@ -53,7 +53,7 @@ def pattern_6_beta(pyre,GreedyStatus,lhs,rhs,LINE_NUM,preb,postb,pre_spc,post_sp
                 res = f"re.search(\"{quantifier}{lhs_1}{pre_res}{lhs_2}{pre_spc}([^ ]+){post_spc}{rhs_1}{str(final_post)}{rhs_2}.*\",TXT)"
                 return res
             elif pyre==False:
-                return f"sed -E -n '{LINE_NUM}s/.*{lhs_1}{pre_res}{lhs_2}{pre_spc}(.+){post_spc}{rhs_1}{str(final_post)}{rhs_2}.*/\\1/p'".format(LINE_NUM)
+                return f"sed -E -n '{LINE_NUM}s/.*{lhs_1}{pre_res}{lhs_2}{pre_spc}(.+){post_spc}{rhs_1}{str(final_post)}{rhs_2}.*/\\1/p'"
     
     elif len(postb.split())==0: #if len is zero for postb
         if pyre==True:
